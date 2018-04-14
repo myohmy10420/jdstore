242e32e7a5a5e795d74e27ecf895218f599c929d
KurtHsu <myohmy10420@gmail.com>
2018-04-14 15:28:35 +0800

    Re add products

---
+++ db/migrate/20180414064142_add_image_to_product2.rb
@@ -0,0 +1,5 @@
+class AddImageToProduct2 < ActiveRecord::Migration[5.0]
+  def change
+    add_column :products, :image, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180412154556) do
+ActiveRecord::Schema.define(version: 20180414064142) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer  "cart_id"
