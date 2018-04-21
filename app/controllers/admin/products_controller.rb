7d793ab45e1a6a6a4f8919b640bf5f4e3e23f76f
KurtHsu <myohmy10420@gmail.com>
2018-04-21 22:03:34 +0800

    Fix admin products controller find order by find -> friendly.find

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -22,11 +22,11 @@ class Admin::ProductsController < ApplicationController
   end
 
   def edit
-    @product = Product.find(params[:id])
+    @product = Product.friendly.find(params[:id])
   end
 
   def update
-    @product = Product.find(params[:id])
+    @product = Product.friendly.find(params[:id])
 
     if @product.update(product_params)
       redirect_to admin_products_path
