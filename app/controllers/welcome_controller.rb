a984cd7187e666aa193e4e138be4cedeaf1488df
KurtHsu <myohmy10420@gmail.com>
2018-04-21 13:28:39 +0800

    Debuging show mutiple commit in root

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 class WelcomeController < ApplicationController
   def index
-    @products_id_1to4 = Product.where(id: 1..4).order("click_rate DESC")
+    @products_id_1to4 = Product.where(id: 1..20).order("click_rate DESC")
   end
 end
