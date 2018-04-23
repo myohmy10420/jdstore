e80b39990b2e4545fe064530ee61c9e1ee0c5cc1
KurtHsu <myohmy10420@gmail.com>
2018-04-23 21:30:52 +0800

    Update landing page

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,2 +1,11 @@
 module WelcomeHelper
+  def render_product_image_link(product)
+    link_to product_path(product) do
+      if product.image.present?
+        image_tag(product.image.medium.url)
+      else
+        image_tag("http://placehold.it/400x400&text=No Pic")
+      end
+    end
+  end
 end
