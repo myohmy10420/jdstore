8c6c91b93f830cf72613a7b0eb887fc6b53ec404
KurtHsu <myohmy10420@gmail.com>
2018-05-03 15:44:45 +0800

    Set up mailgun

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,8 +51,13 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }
-  config.action_mailer.delivery_method = :letter_opener
+  # config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  # config.action_mailer.delivery_method = :letter_opener
+
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = config_for(:email).symbolize_keys
+
 
   config.after_initialize do
     Pay2go.integration_mode = :development
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,8 +83,10 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
   config.action_mailer.default_url_options = { host: 'kurt-jdstroe.heroku.com' }
-  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = config_for(:email).symbolize_keys
 
   config.after_initialize do
     Pay2go.integration_mode = :production
