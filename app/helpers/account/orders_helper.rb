98b55ff226cbd592a6f60d93d391c7e6d1875e43
KurtHsu <myohmy10420@gmail.com>
2018-05-05 01:56:56 +0800

    Enhacement order state

--- app/helpers/account/orders_helper.rb
+++ app/helpers/account/orders_helper.rb
@@ -1,2 +1,20 @@
 module Account::OrdersHelper
+  def render_order_human_state(order)
+    case order.aasm_state
+    when "order_placed"
+      content_tag(:span, "已下單", class: "label label-default")
+    when "paid"
+      "已付款"
+    when "shipping"
+      "處理中"
+    when "shipped"
+      "處理完畢"
+    when "order_cancelled"
+      "訂單取消"
+    when "good_returned"
+      "訂單完成"
+    else
+      "Unknown"
+    end
+  end
 end
