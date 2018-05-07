1aa13ff8851ece90d26a3c66348fb143267f169b
KurtHsu <myohmy10420@gmail.com>
2018-05-07 22:30:38 +0800

    update product#index view and build seed.rb

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,11 +1,9 @@
 module ProductsHelper
-  def reder_production_image
-    link_to product_path(product) do
-      if product.image.present?
-        image_tag(product.image.thumb.url, class: "thumbnail")
-      else
-        image_tag("http://placehold.it/200x200&text=No Pic", class: "thumbnail")
-      end
+  def render_production_image_path(product)
+    if product.image.present?
+      image_tag(product.image.thumb.url, class: "thumbnail", style: "margin: 20px auto;")
+    else
+      image_tag("http://placehold.it/200x200&text=No Pic", class: "thumbnail", style: "margin: 20px auto;")
     end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,24 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-u = User.new
-u.email = "isadmin2@test.com"           # 可以改成自己的 email
+# u = User.new
+# u.email = "myohmy10420@gmail.com"
 
-u.password = "123456"                # 最少要六码
+# u.password = "111111"
 
-u.password_confirmation = "123456"   # 最少要六码
+# u.password_confirmation = "111111"
 
-u.is_admin = true
-u.save
+# u.is_admin = true
+# u.save
+
+creator_id = User.find_by(email: "myohmy10420@gmail.com").try(:id) || 1
+
+Product.create!(
+  title: "Macbook pro",
+  description: "蘋果筆電",
+  quantity: 2,
+  price: 32000,
+  image: open("https://i.guim.co.uk/img/media/4371417c1b5e8fab6bb9f05904d260d40dc18f82/133_0_4175_2505/master/4175.jpg?w=1920&q=55&auto=format&usm=12&fit=max&s=191e5d87d7c6fce47bb31e7bcc9ad5cd")
+)
+
+puts "Added Macbook pro"
