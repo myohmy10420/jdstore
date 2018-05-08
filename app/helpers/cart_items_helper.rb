f892d0a45cdecae8c28379cfd9e3a9aa09153634
KurtHsu <myohmy10420@gmail.com>
2018-05-08 21:02:12 +0800

    Update cart view and change all content to TW laguage

--- app/helpers/cart_items_helper.rb
+++ app/helpers/cart_items_helper.rb
@@ -1,2 +1,5 @@
 module CartItemsHelper
+  def render_item_total_price(cart_item)
+    cart_item.total_price(cart_item)
+  end
 end
--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -2,4 +2,8 @@ class CartItem < ApplicationRecord
   belongs_to :cart
   belongs_to :product
 
+  def total_price(cart_item)
+    production = Product.find(cart_item.product_id)
+    production.price*cart_item.quantity
+  end
 end
